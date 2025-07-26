fa760020ce4654dbd840bbbf40961c91cd29eaef
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2025-07-26 09:20:41 +0900

    Header再度取り付け

--- src/pages/AddMemo/AddMemo.jsx
+++ src/pages/AddMemo/AddMemo.jsx
@@ -46,6 +46,10 @@ const AddMemo = () => {
 
     return (
         <div className={styles.container}>
+            <Header
+                title={`優先度: ${priority}`}
+                onBack={() => navigate(-1)}
+            />
 
             {/* ✅ 現在時刻表示 */}
             <div className={styles.currentTime}>
