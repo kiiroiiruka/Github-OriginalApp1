dd9e766d3889bb7f0b9ac1ddf9efb2ada2a45169
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2026-07-17 13:22:56 +0900

    feat:firebaseフォルダ、env設定ファイル設置

---
+++ firebase/config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';//firebaseを使用するためのモジュールをインポート。
+import { getFirestore } from 'firebase/firestore';//firestoreを使用するためのモジュールをインポート。
+import { getStorage } from 'firebase/storage';//storageを使用するためのモジュールをインポート。
+
+//firebaseのAPIキーを.envファイルから取得してセットさせる。
+//デプロイ後はNetlifyにセットされた環境変数がセットされて動作する。
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+//firebaseを使用するためのオブジェクトを作成してappに代入。
+//以降でappを使用しfirebaseの操作を可能にするオブジェクトの作成をしている。
+const app = initializeApp(firebaseConfig);
+
+
+//firestoreオブジェクトを作成してdbに代入。呼び出してfirestoreを使用。
+export const db = getFirestore(app);
+//storageを使うためのオブジェクトを作成している。呼び出して画像データを保存できる。
+export const storage = getStorage(app);
+export default app;
---
+++ firebase/index.js
@@ -0,0 +1,1 @@
+export { default as app, db, storage } from './config.js';
